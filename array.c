5d041f5d94f90d04db52ccb41ffea6346bd37823
Supratim Samantray <supratim.samantray@gmail.com>
2022-01-26 16:56:00 +0530

    Add function to accept input and to display array contents

--- array.c
+++ array.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #define MAX 16
 
+void accept_input(int *arr, int n) {
+   int i;
+   for (i = 0; i < n; ++i) 
+   scanf("%d", &arr[i]);
+}
+
+void display(int *arr, int n) {
+   int i;
+   for (i = 0; i < n; ++i) 
+   printf("|%d| ", arr[i]);
+   
+   printf("\n");
+}
+
 int main(void) {
    int i, n, arr[MAX];
    printf("Enter the total number of elements: ");
